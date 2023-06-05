0ae54687a2659fe76adee4de1adbfbe63f0c68b4
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-06-05 18:31:05 +0530

    initial commit

---
+++ tipCalculator.js
@@ -0,0 +1,10 @@
+function calculateTotal() {
+  let billAmount = Number(document.getElementById("bill").value);
+  let tip = Number(document.getElementById("tip").value);
+
+  let tipPercentage = tip/100;
+  let totalBillAmount = billAmount + tipPercentage;
+
+  let totalElement = document.getElementById("total");
+  totalElement.innerHTML = `Total bill amount: ₹${totalBillAmount.toFixed(2)}`;
+}
